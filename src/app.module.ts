008fb70021c42472d5d4b7ce0bd4a291c0b392f1
gkaragoz <gkaragoz96@gmail.com>
2020-08-12 16:49:32 +0300

    Add mongo atlas cloud URI. Finalize project.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { ProductModule } from './product/product.module';
 import { TypegooseModule } from 'nestjs-typegoose';
 import 'dotenv/config';
 
-console.log(process.env);
-
 @Module({
   imports: [
-    TypegooseModule.forRoot(process.env.MONGO_URI, { useFindAndModify: false }),
+    TypegooseModule.forRoot(process.env.MONGO_URI, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
     ProductModule,
   ],
   controllers: [AppController],
